07df80b413880e2e83d1d641f0ff78817ff6bb2b
Srijan Dhungana <srijan.dhungana6@gmail.com>
2023-12-28 20:57:42 +0200

    Add support for loading Wavefront OBJ files

--- WavefrontLoader.h
+++ WavefrontLoader.h
@@ -20,6 +20,19 @@ struct Index
     uint32_t vtIdx = 0;
 };
 
+struct Vec3
+{
+    float x = 0.0f;
+    float y = 0.0f;
+    float z = 0.0f;
+};
+
+struct Vec2
+{
+    float x = 0.0f;
+    float y = 0.0f;
+};
+
 /// @brief Mesh in a scene.
 struct Mesh
 {
@@ -31,13 +44,13 @@ struct Mesh
     std::string Name = "";
 
     /// @brief Positions of the vertices.
-    std::vector<float> Positions = {};
+    std::vector<Vec3> Positions = {};
 
     /// @brief Normals of the vertices.
-    std::vector<float> Normals = {};
+    std::vector<Vec3> Normals = {};
 
     /// @brief Texture coordinates of the vertices.
-    std::vector<float> TexCoords = {};
+    std::vector<Vec2> TexCoords = {};
 
     /// @brief Indices of the vertices.
     std::vector<Index> Indices = {};
@@ -53,12 +66,13 @@ struct Scene
     // std::vector<Material> Materials = {};
 };
 
-/// @brief Wavefront OBJ file loader.
+/// @brief Wavefront OBJ file loader. Only supports triangulated meshes and doesn't support negative indices.
 class WavefrontLoader
 {
 private:
     enum class FaceType
     {
+        None,
         Vertex,
         VertexNormal,
         VertexTextureNormal,
@@ -67,7 +81,7 @@ private:
 
 public:
     WavefrontLoader() = default;
-    ~WavefrontLoader();
+    ~WavefrontLoader() = default;
 
     /// @brief Loads a scene from a file.
     /// @param path Path to the file.
@@ -82,8 +96,7 @@ public:
 
         Scene outScene;
 
-        bool slashesCheck = false;
-        FaceType faceType = FaceType::Vertex;
+        FaceType faceType = FaceType::None;
 
         for (std::string line; std::getline(file, line);)
         {
@@ -101,9 +114,9 @@ public:
                 // Group
             }
 
-            if (outScene.Meshes.empty() || line[0] == 'o' || line[0] == 'g')
+            if (outScene.Meshes.empty() || line[0] == 'o')
             {
-                outScene.Meshes.push_back(Mesh());
+                outScene.Meshes.push_back(Mesh(line.substr(2)));
             }
 
             Mesh& mesh = outScene.Meshes.back();
@@ -112,54 +125,154 @@ public:
             {
                 if (line[1] == ' ')
                 {
+                    Vec3 position = mesh.Positions.emplace_back();
 
-                    int n = std::sscanf(line.c_str(), "v %f %f %f", &mesh.Positions.emplace_back(),
-                                        &mesh.Positions.emplace_back(), &mesh.Positions.emplace_back());
+                    int n = std::sscanf(line.c_str(), "v %f %f %f", &position.x, &position.y, &position.z);
 
                     assert(n == 3);
                 }
                 else if (line[1] == 'n')
                 {
-                    int n = std::sscanf(line.c_str(), "vn %f %f %f", &mesh.Normals.emplace_back(),
-                                        &mesh.Normals.emplace_back(), &mesh.Normals.emplace_back());
+                    Vec3& normal = mesh.Normals.emplace_back();
+
+                    int n = std::sscanf(line.c_str(), "vn %f %f %f", &normal.x, &normal.y, &normal.z);
 
                     assert(n == 3);
                 }
                 else if (line[1] == 't')
                 {
-                    int n = std::sscanf(line.c_str(), "vt %f %f", &mesh.TexCoords.emplace_back(),
-                                        &mesh.TexCoords.emplace_back());
+                    Vec2& texCoord = mesh.TexCoords.emplace_back();
+
+                    int n = std::sscanf(line.c_str(), "vt %f %f", &texCoord.x, &texCoord.y);
 
                     assert(n == 2);
                 }
             }
             else if (line[0] == 'f')
             {
-                if (!slashesCheck)
+                if (faceType == FaceType::None)
                 {
-                    if (std::ranges::count(line, '/') == 0)
-                    {
-                        faceType = FaceType::Vertex;
-                    }
-                    else if (std::ranges::count(line, '/') == 3)
-                    {
-                        faceType = FaceType::VertexTextureNormal;
-                    }
-                    else if (std::ranges::count(line, '/') == 6)
-                    {
-                        faceType = FaceType::VertexTexture;
-                    }
-                    else if (std::ranges::count(line, '/') == 9)
-                    {
-                        faceType = FaceType::VertexNormal;
-                    }
-                    else
-                    {
-                        assert(false);
-                    }
-
-                    slashesCheck = true;
+                    faceType = DetermineFaceType(line);
                 }
+
+                mesh.Indices.emplace_back(ParseIndex(line, faceType));
+            }
+        }
+    }
+
+    /// @brief Convert a mesh to a continous array of vertices so that it can be used with graphics APIs.
+    /// The layout of the vertices is as follows:
+    /// - Position (3 floats)
+    /// - Normal (3 floats) if the mesh has normals
+    /// - Texture coordinate (2 floats) if the mesh has texture coordinates
+    /// - Additional data (N floats) if you reserved N floats for each vertex
+    /// Total stride is 3 + (hasNormals ? 3 : 0) + (hasTexCoords ? 2 : 0) + reserve_floats.
+    /// @param mesh Mesh to convert.
+    /// @param reserve_floats Number of floats to reserve for each vertex so that you can add additional data to each
+    /// vertex. For example, if you want to add a color (3 floats) to each vertex, you would pass 3 here.
+    /// @return The vertices of the mesh.
+    std::vector<float> ConvertToStrided(Mesh const& mesh, uint32_t reserve_floats = 0)
+    {
+
+        assert(mesh.Positions.size() % 3);
+        assert(mesh.Positions.size() == mesh.Normals.size());
+        assert(mesh.Indices.size() % 3 == 0);
+
+        std::vector<float> vertices(mesh.Positions.size() + mesh.Normals.size() + mesh.TexCoords.size());
+
+        const bool hasNormals = !mesh.Normals.empty();
+        const bool hasTexCoords = !mesh.TexCoords.empty();
+
+        const uint32_t stride = 3 + (hasNormals ? 3 : 0) + (hasTexCoords ? 2 : 0) + reserve_floats;
+
+        for (Index const& index : mesh.Indices)
+        {
+            uint8_t* data = reinterpret_cast<uint8_t*>(&vertices[index.vIdx * stride]);
+
+            memcpy(data, &mesh.Positions[index.vIdx], sizeof(Vec3));
+
+            data += sizeof(Vec3);
+
+            if (hasNormals)
+            {
+                memcpy(data, &mesh.Normals[index.vnIdx], sizeof(Vec3));
+
+                data += sizeof(Vec3);
+            }
+            if (hasTexCoords)
+            {
+                memcpy(data, &mesh.TexCoords[index.vtIdx], sizeof(Vec2));
+            }
+        }
+        return std::move(vertices);
+    }
+
+private:
+    Index ParseIndex(const std::string& line, FaceType faceType)
+    {
+        Index outIndex;
+
+        if (faceType == FaceType::Vertex)
+        {
+            int n = std::sscanf(line.c_str(), "f %d %d %d", &outIndex.vIdx, &outIndex.vIdx, &outIndex.vIdx);
+
+            assert(n == 3);
+        }
+        else if (faceType == FaceType::VertexNormal)
+        {
+            int n = std::sscanf(line.c_str(), "f %d//%d %d//%d %d//%d", &outIndex.vIdx, &outIndex.vnIdx, &outIndex.vIdx,
+                                &outIndex.vnIdx, &outIndex.vIdx, &outIndex.vnIdx);
+
+            assert(n == 6);
+        }
+        else if (faceType == FaceType::VertexTextureNormal)
+        {
+            int n = std::sscanf(line.c_str(), "f %d/%d/%d %d/%d/%d %d/%d/%d", &outIndex.vIdx, &outIndex.vtIdx,
+                                &outIndex.vnIdx, &outIndex.vIdx, &outIndex.vtIdx, &outIndex.vnIdx, &outIndex.vIdx,
+                                &outIndex.vtIdx, &outIndex.vnIdx);
+
+            assert(n == 9);
+        }
+        else if (faceType == FaceType::VertexTexture)
+        {
+            int n = std::sscanf(line.c_str(), "f %d/%d %d/%d %d/%d", &outIndex.vIdx, &outIndex.vtIdx, &outIndex.vIdx,
+                                &outIndex.vtIdx, &outIndex.vIdx, &outIndex.vtIdx);
+
+            assert(n == 6);
+        }
+    }
+
+    FaceType DetermineFaceType(const std::string& line)
+    {
+
+        bool doubleSlashes = line.find("//") != std::string::npos;
+        bool slashes = line.find("/") != std::string::npos;
+
+        // If there are no slashes, the face type is Vertex.
+        // eg. f 1 2 3
+        if (!doubleSlashes && !slashes)
+        {
+            return FaceType::Vertex;
+        }
+        // If there are double slashes, the face type is VertexNormal.
+        // eg. f 1//1 2//2 3//3
+        else if (doubleSlashes)
+        {
+            return FaceType::VertexNormal;
+        }
+        else if (slashes)
+        {
+            // If there are no double slashes, the face type is VertexTextureNormal.
+            // eg. f 1/1/1 2/2/2 3/3/3
+            if (!doubleSlashes)
+            {
+                return FaceType::VertexTextureNormal;
+            }
+            // There are no double slashes, but there are slashes, so the face type is VertexTexture.
+            // eg. f 1/1 2/2 3/3
+            else
+            {
+                return FaceType::VertexTexture;
             }
         }
     }
